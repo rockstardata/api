645a15071a236764d3225e0c3508e8111acc9c4e
ricardodeveloper <128541735+aleRiki@users.noreply.github.com>
2025-07-16 12:18:37 -0400

    cors correcions

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 // }
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
 
   app.useGlobalPipes(
     new ValidationPipe({
